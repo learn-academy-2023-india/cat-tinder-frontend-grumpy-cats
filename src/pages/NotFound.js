e6580ff4db650017a9976d3b8e48736fd960a1df
RoderickVongsa <roderick.vongsa@gmail.com>
2024-02-07 13:41:00 -0800

    frontend structure fix

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,24 +2,18 @@ import React from 'react';
 import gif from '../assets/wtfcat.gif';
 
 const NotFoundPage = () => {
-  const containerStyle = {
-    backgroundImage: `url(${gif})`,
-    backgroundSize: 'cover',
-    backgroundColor: '#0000FF',
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    color: 'white',
-    fontSize: '2rem',
-  };
+ 
 
   return (
-    <div style={containerStyle}>
-      <h1>Page not found, no single cats in this neighborhood</h1>
+    <>
+        <img
+            src={gif}
+            alt="Gif not found"
+            className="wtf-cat"
+          />
+      <h3 className="notfoundtext">Page not found, no single cats in this neighborhood!</h3>
      
-    </div>
+    </>
   );
 };
 
