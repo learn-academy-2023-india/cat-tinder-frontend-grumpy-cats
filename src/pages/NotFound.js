53124184c2b1d0cdcc22250fa97dffc9d85727d7
RoderickVongsa <roderick.vongsa@gmail.com>
2024-02-07 13:54:19 -0800

    frontend structure

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,19 +2,16 @@ import React from 'react';
 import gif from '../assets/wtfcat.gif';
 
 const NotFoundPage = () => {
- 
-
   return (
     <>
         <img
             src={gif}
             alt="Gif not found"
             className="wtf-cat"
-          />
-      <h3 className="notfoundtext">Page not found, no single cats in this neighborhood!</h3>
-     
+        />
+        <h3 className="notfoundtext">Page not found, no single cats in this neighborhood!</h3>
     </>
-  );
-};
+  )
+}
 
 export default NotFoundPage;
