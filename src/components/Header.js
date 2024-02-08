846b7b057205c3b3ce6a00c8f4e04abe7deea569
chantelcarter <chantel.alexia@gmail.com>
2024-02-07 16:31:05 -0800

    removed unused code

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,25 +33,3 @@ const Header = () => {
 }
 
 export default Header
-
-
-// <>
-    //     <header>
-    //         <img
-    //         src={catLogo}
-    //         alt="Cat Tinder logo with outline of cat"
-    //         className="cat-logo"
-    //       />
-    //       <p>"Come get your PURRRR on! Find that special kittyCat to share your catbed!" -Lio 
-    //       </p>
-    //     </header>
-    // </>
-
-
-
-
-
-
-
-
-
