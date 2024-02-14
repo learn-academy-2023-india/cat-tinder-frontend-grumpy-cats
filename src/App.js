e6f340a6ced3bce9c559960014d114b7de611093
chantelcarter <chantel.alexia@gmail.com>
2024-02-14 14:44:31 -0800

    updated web link

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from "react"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import CatEdit from "./pages/CatEdit"
-import CatIndex from "./pages/CatIndex"
-import CatNew from "./pages/CatNew"
-import CatShow from "./pages/CatShow"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import { Routes, Route } from "react-router-dom"
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CatEdit from "./pages/CatEdit";
+import CatIndex from "./pages/CatIndex";
+import CatNew from "./pages/CatNew";
+import CatShow from "./pages/CatShow";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
-  const [cats, setCats] = useState([])
+  const [cats, setCats] = useState([]);
 
   useEffect(() => {
-    readCat()
-  }, [])
+    readCat();
+  }, []);
 
   const readCat = () => {
-    fetch("http://localhost:3000/cats")
+    fetch("https://cat-tinder-wwi3.onrender.com/cats")
       .then((response) => response.json())
       .then((data) => setCats(data))
-      .catch((error) => console.log(error))
-  }
+      .catch((error) => console.log(error));
+  };
 
   const createCat = (newCat) => {
-    fetch("http://localhost:3000/cats", {
+    fetch("https://cat-tinder-wwi3.onrender.com/cats", {
       body: JSON.stringify(newCat),
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      method: "POST"
+      method: "POST",
     })
       .then((response) => response.json())
       .then(() => readCat())
-      .catch((errors) => console.log("Cat create errors:", errors))
-}
+      .catch((errors) => console.log("Cat create errors:", errors));
+  };
 
   return (
     <>
-   <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/catindex" element={<CatIndex cats ={cats}/>} />
+        <Route path="/catindex" element={<CatIndex cats={cats} />} />
         <Route path="/catshow/:id" element={<CatShow cats={cats} />} />
         <Route path="/catnew" element={<CatNew createCat={createCat} />} />
         <Route path="/catedit" element={<CatEdit />} />
@@ -50,7 +50,7 @@ const App = () => {
       </Routes>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
